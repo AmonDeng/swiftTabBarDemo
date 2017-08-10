c11ab652010e013eb050d870f2efd506d6a10ff2
Amon Deng <amon@AmondeiMac.local>
2017-08-10 13:46:02 +0800

    demo

---
+++ DMSwitTabbarController/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DMSwitTabbarController
+//
+//  Created by Amon Deng on 2017/8/10.
+//  Copyright © 2017年 Amon Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DMSwitTabbarController/testViewController.swift
@@ -0,0 +1,26 @@
+//
+//  testViewController.swift
+//  DMSwitTabbarController
+//
+//  Created by Amon Deng on 2017/8/10.
+//  Copyright © 2017年 Amon Deng. All rights reserved.
+//
+
+
+import UIKit
+
+class testViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
